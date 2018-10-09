0eeec204296886d55912c80d027bb1f2547e37ff
Kelly Sheddy <jungaka07@gmail.com>
2018-10-09 23:25:22 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,7 +60,7 @@ def game_hash
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => {
-      "Jeff Adrien" => {
+      "Jeff Adrien", => {
         :number => 4, 
         :shoe => 18,
         :points => 10, 
@@ -70,7 +70,7 @@ def game_hash
         :blocks => 7,
         :slam_dunks => 2  
       },
-      "Bismak Biyombo" => {
+      "Bismak Biyombo", => {
         :number => 0, 
         :shoe => 16, 
         :points => 12, 
